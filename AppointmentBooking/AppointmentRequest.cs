61a4b52d1855f4bd06663a2ea65bc46889ef0177
Hita <ftx0223@autuni.ac.nz>
2026-07-22 16:14:43 +1200

    Add basic appointment booking system and baseline tests

---
+++ AppointmentBooking/AppointmentRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ENSE707_AppointmentBooking
+{
+    public class AppointmentRequest
+    {
+        public Patient Patient { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime RequestedDate { get; set; }
+    }
+}
